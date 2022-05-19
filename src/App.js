fd2461f839bc1cbf2ed3ae494a782b866c65423d
unknown <saradawsone@gmail.com>
2022-05-18 21:10:51 -0700

    stretch goal 1 started

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Image from "./components/image";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 
 function App() {
   const [nasaData, setNasaData] = useState();
+  const[startDate, setStartDate] = useState(new Date());
   
+  console.log(startDate);
+
+  const startDateArr = startDate.toString().split(' ');
+  const year = startDateArr[3];
+    let monthNum = ''
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    months.map(month => {
+      if(month === startDateArr[1]){
+         return monthNum = `0${months.indexOf(month)}`
+      }
+    })
+
+  const day = startDateArr[2]
+
+  const date = `${year}-${monthNum}-${day}`;
 
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=NiyTpJoHMZzbw8yxclnJfk3ceeA0Th5dYvULfpGq')
+  axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
     .then(res => {
       setNasaData(res.data);
     }).catch(err => {
@@ -20,7 +38,8 @@ function App() {
 
   return (
     <div className="App">
-        <h1>NASA Photo Of The Day</h1>
+      <h1>NASA Photo Of The Day</h1>
+      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}/>
       { nasaData && <Image image={nasaData}/> }
     </div>
   );
