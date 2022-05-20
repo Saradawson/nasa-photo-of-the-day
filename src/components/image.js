d6d0f52cda30de19a8b5c04ebb6f088765c6d9f0
unknown <saradawsone@gmail.com>
2022-05-19 22:25:59 -0700

    looking pretty

--- src/components/image.js
+++ src/components/image.js
@@ -1,13 +1,42 @@
 import React from "react";
+import styled from 'styled-components';
+
+const PhotoWraper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1% 5%;
+    font-size: 1em;
+    margin: 1em;
+    border: solid tomato;
+    background: black;
+    font-family: Arial, sans-serif;
+    align-items: center;
+    color: white; 
+`;
+
+const NasaIMG = styled.img`
+    width: 100%;
+    border: solid grey;
+`;
+
+const PhotoParagraph = styled.p`
+    width: 95%;
+    padding: 1%;
+`;
+
+const PhotoHeader = styled.h2`
+    margin-bottom: 1%;
+`;
+
 
 const Image = (props) => {
     return (
-        <div className="photo-wraper">
-            <h2>{props.image.title}</h2>
+        <PhotoWraper className="photo-wraper">
+            <PhotoHeader>{props.image.title}</PhotoHeader>
             <p>{props.image.date}</p>
-            <img src={props.image.hdurl}/>
-            <p>{props.image.explanation}</p>
-        </div>
+            <NasaIMG src={props.image.hdurl} />
+            <PhotoParagraph>{props.image.explanation}</PhotoParagraph>
+        </PhotoWraper>
     )
 }
 
